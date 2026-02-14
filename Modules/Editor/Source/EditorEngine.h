10b5e6b6fa1661478f4dac35ed19bfb9e0419279
YanSchw <schoessow.yannick@gmail.com>
2026-02-14 02:13:23 +0100

    Added Editor and Runtime skeleton

---
+++ Modules/Editor/Source/EditorEngine.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Core/Engine.h"
+#include "EditorEngine.gen.h"
+
+class EditorEngine : public Engine {
+public:
+    ARTIFACT_CLASS();
+};
---
+++ Modules/Runtime/Source/GameEngine.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Core/Engine.h"
+#include "GameEngine.gen.h"
+
+class GameEngine : public Engine {
+public:
+    ARTIFACT_CLASS();
+};
