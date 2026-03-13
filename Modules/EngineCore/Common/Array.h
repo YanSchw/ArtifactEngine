4710e7894e825e76ba09ef36d7314a6ffdd9a8b4
YanSchw <schoessow.yannick@gmail.com>
2026-03-14 00:47:21 +0100

    Added Array and Map types

--- Modules/EngineCore/Common/Array.h
+++ Modules/EngineCore/Common/Array.h
@@ -1,8 +1,144 @@
 #pragma once
 #include <vector>
+#include <iterator>
 
-template<typename T>
-struct Array : public std::vector<T> {
+struct TArray {
+
+};
+
+template <class T>
+class Array {
+private:
+    TArray m_BaseArray;
+    std::vector<T> m_Data;
+
 public:
-    using std::vector<T>::vector; // Inherit all constructors
+
+    Array() {
+        m_Data = std::vector<T>();
+    }
+    Array(size_t size) {
+        m_Data = std::vector<T>(size);
+    }
+    Array(const std::vector<T>& vector) {
+        m_Data = vector;
+    }
+    Array(const std::initializer_list<T>& vector) {
+        m_Data = vector;
+    }
+
+    void Emplace(T item) {
+        return m_Data.emplace_back(item);
+    }
+    void Add(T&& item) {
+        m_Data.push_back(item);
+    }
+    void Add(const T& item) {
+        m_Data.push_back(item);
+    }
+
+    void Insert(int32_t index, T& item) {
+        m_Data.insert(m_Data.begin() + index, item);
+    }
+
+    void SwapElements(int32_t indexA, int32_t indexB) {
+        std::iter_swap(m_Data.begin() + indexA, m_Data.begin() + indexB);
+    }
+
+    inline T& Get(int32_t index) {
+        return m_Data[index];
+    }
+    inline T& FirstItem() {
+        return m_Data[0];
+    }
+    inline T& SecondItem() {
+        return m_Data[1];
+    }
+    inline T& LastItem() {
+        return m_Data[Last()];
+    }
+    inline T& operator[](int32_t index) {
+        return m_Data[index];
+    }
+    inline const T& operator[](int32_t index) const {
+        return m_Data[index];
+    }
+
+    int32_t IndexOf(const T& item) {
+        auto it = std::find(m_Data.begin(), m_Data.end(), item);
+
+        if (it != m_Data.end()) return (int32_t)(it - m_Data.begin());
+        else return -1;
+    }
+
+    void RemoveAt(int32_t index) {
+        m_Data.erase(m_Data.begin() + index);
+    }
+    void Remove(const T& item) {
+        RemoveAt(IndexOf(item));
+    }
+    inline void RemoveFirstItem() {
+        RemoveAt(0);
+    }
+    inline void RemoveSecondItem() {
+        RemoveAt(1);
+    }
+    inline void RemoveLastItem() {
+        RemoveAt(Last());
+    }
+
+    bool Contains(const T& item) const {
+        return std::find(m_Data.begin(), m_Data.end(), item) != m_Data.end();
+    }
+
+    Array<T> operator+(Array<T>& other) {
+        std::vector<T> AB;
+        AB.reserve(m_Data.size() + other.GetData().size()); // preallocate memory
+        AB.insert(AB.end(), m_Data.begin(), m_Data.end());
+        AB.insert(AB.end(), other.GetData().begin(), other.GetData().end());
+        return { AB };
+    }
+    void operator+=(Array<T>& other) {
+        *this = *this + other;
+    }
+
+    void Clear() {
+        m_Data.clear();
+    }
+
+    inline int32_t Size() const {
+        return (int32_t)m_Data.size();
+    }
+    // Returns the last valid index [ Size() - 1 ]
+    inline int32_t Last() const {
+        return Size() - 1;
+    }
+
+    bool IsEmpty() const {
+        return m_Data.empty();
+    }
+
+    std::vector<T>& GetData() {
+        return m_Data;
+    }
+
+    void SetData(std::vector<T>& data) {
+        this->m_Data = data;
+    }
+
+    // Ascending: a < b
+    // Descending: a > b
+    void Sort(const std::function<bool(const T&, const T&)>& lambda) {
+        std::ranges::sort(m_Data, lambda);
+    }
+
+    // Iterators
+
+    typename std::vector<T>::iterator begin() { return m_Data.begin(); }
+    typename std::vector<T>::const_iterator begin() const { return m_Data.begin(); }
+    typename std::vector<T>::const_iterator cbegin() const { return m_Data.cbegin(); }
+    typename std::vector<T>::iterator end() { return m_Data.end(); }
+    typename std::vector<T>::const_iterator end() const { return m_Data.end(); }
+    typename std::vector<T>::const_iterator cend() const { return m_Data.cend(); }
+
 };
---
+++ Modules/EngineCore/Common/Map.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <unordered_map>
+#include <iterator>
+
+template<class K, class V>
+class Map {
+    std::unordered_map<K, V> m_Map;
+public:
+
+    bool ContainsKey(const K& key) const {
+        return m_Map.find(key) != m_Map.end();
+    }
+
+    void Remove(const K& key) {
+        m_Map.erase(key);
+    }
+
+    V& operator[](const K& key) {
+        return m_Map[key];
+    }
+    V& operator[](K&& key) {
+        return m_Map[key];
+    }
+    const V& At(const K& key) {
+        return m_Map.at(key);
+    }
+    V GetOrDefault(const K& key, V&& InDefault) {
+        return ContainsKey(key) ? At(key) : InDefault;
+    }
+
+    void Clear() {
+        m_Map.clear();
+    }
+
+    size_t Size() {
+        return m_Map.size();
+    }
+
+    // Iterators
+
+    typename std::unordered_map<K, V>::iterator begin() { return m_Map.begin(); }
+    typename std::unordered_map<K, V>::const_iterator begin() const { return m_Map.begin(); }
+    typename std::unordered_map<K, V>::const_iterator cbegin() const { return m_Map.cbegin(); }
+    typename std::unordered_map<K, V>::iterator end() { return m_Map.end(); }
+    typename std::unordered_map<K, V>::const_iterator end() const { return m_Map.end(); }
+    typename std::unordered_map<K, V>::const_iterator cend() const { return m_Map.cend(); }
+};
