1ae7ec9324b7571273d0efb29a8a97de7f3d1d46
YanSchw <schoessow.yannick@gmail.com>
2026-03-24 23:03:16 +0100

    Added String

--- Modules/EngineCore/Common/String.h
+++ Modules/EngineCore/Common/String.h
@@ -1,8 +1,4 @@
 #pragma once
 #include <string>
 
-// STL wrapper around std::string to allow extensions.
-struct String : public std::string {
-public:
-    using std::string::string; // Inherit all constructors
-};
+using String = std::string;
