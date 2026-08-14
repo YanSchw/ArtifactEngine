ed402df44c367d1dea0b4de9f14c7eaedd331b1f
YanSchw <schoessow.yannick@gmail.com>
2026-08-14 18:08:25 +0200

    Added Icons for UINodes

--- Modules/Editor/Source/UI/EditorIcons.cpp
+++ Modules/Editor/Source/UI/EditorIcons.cpp
@@ -28,6 +28,21 @@ VectorImage* EditorIcons::GetNodeIcon(const Class& InClass) {
         s_NodeIcons["CylinderShapeNode"] = CylinderShape();
         s_NodeIcons["RigidBodyNode"] = RigidBody();
         s_NodeIcons["CharacterNode"] = Character();
+        s_NodeIcons["UINode"] = Get("d5ff8074-f233-41a8-b202-f6ced99f2d8f");
+        s_NodeIcons["UICanvas"] = Get("26818e74-f484-4f52-8f97-b06dcb64c58b");
+        s_NodeIcons["UIQuad"] = Get("62670d8d-e97b-4c2e-afdd-3f69ebbddb83");
+        s_NodeIcons["UILabel"] = Get("177e97c2-406e-4cd6-8642-725b023d56db");
+        s_NodeIcons["UIButton"] = Get("310550f5-b61a-4785-bebf-7fafbd2bcfdc");
+        s_NodeIcons["UIImage"] = Get("ffd11530-3e99-465a-9bc7-977bd99eda64");
+        s_NodeIcons["UISvg"] = Get("ab4214ae-5b25-4488-a8fb-2795ac6ccc1d");
+        s_NodeIcons["UIStack"] = Get("db37f8bf-0d81-48b2-83eb-ed2008128b39");
+        s_NodeIcons["UIHStack"] = Get("b0499709-b4a7-4f1c-8657-98d93b312e36");
+        s_NodeIcons["UIVStack"] = Get("e76ce95f-e3fb-4d95-9bc8-7d890d4295a5");
+        s_NodeIcons["UIScrollArea"] = Get("f086650c-44a5-4861-8051-fc62d6a820c2");
+        s_NodeIcons["UITextArea"] = Get("536de86e-7d42-4604-8d9d-e7b2d25ab405");
+        s_NodeIcons["UIToggle"] = Get("c2d48cb1-c5bf-4b93-bb8d-2b7f41919506");
+        s_NodeIcons["UIForEach"] = Get("ed702f8c-5e30-4ee3-9ec9-da846f54fef5");
+        s_NodeIcons["UIIf"] = Get("34ebcbd5-8be0-457c-88c3-874d6ece94a4");
     }
 
     VectorImage* icon = nullptr;
